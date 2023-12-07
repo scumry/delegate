25d4727156c32c7adb555b56b01efcb89da16d9f
scumry <diana.ma1310@mail.ru>
2023-12-07 23:26:48 +0500

    &

--- delegate.cs
+++ delegate.cs
@@ -1,33 +1,4 @@
-﻿class Program
-{
-    static void Main()
-    {
-        Calculator calculator = new Calculator();
-        Tachies tack = new Tachies(calculator.Min);
-        tack = new Tachies(calculator.Plus);
-        Tachies tacs = new Tachies(calculator.Del);
-        tacs = new Tachies(calculator.Ymn);
-        Console.WriteLine("Вычитание - :"+tack(10, 2));
-        Console.WriteLine("Вычитание - :"+tack(10,2));
-        Console.WriteLine("Деление / :"+tacs(10,2));
-        Console.WriteLine("Деление / :"+tacs(10, 2));
-    }
-}
-class Program
-{
-    static void Main()
-    {
-        Calculator calculator = new Calculator();
-        Tachies tack = new Tachies(calculator.Add);
-        tack = new Tachies(calculator.Subtract);
-        Tachies tacs = new Tachies(calculator.Multiply);
-        tacs = new Tachies(calculator.Divide);
-        Console.WriteLine("Уаначале + :"+tack(15, 11));
-        Console.WriteLine("Потоме - :"+tack(151,555));
-        Console.WriteLine("Усередине * :"+tacs(231,151));
-        Console.WriteLine("Уконце / :"+tacs(123, 151));
-    }
-}
+﻿
 ///___
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
